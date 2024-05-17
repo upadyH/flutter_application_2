14bbf1d1c9517c2379a610bc5f988fcde76c0b8b
Upady Hatthasin <uht@rmutl.ac.th>
2024-05-17 10:01:48 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class _MyHomeSreenState extends StatefulWidget {
+  const _MyHomeSreenState({super.key});
+
+  @override
+  State<_MyHomeSreenState> createState() => __MyHomeSreenStateState();
+}
+
+class __MyHomeSreenStateState extends State<_MyHomeSreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/sreen/home_sreen.dart
@@ -0,0 +1,43 @@
+```
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Example 3"),
+      ),
+      body: const Row( mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.call),
+              Text("Call"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.route),
+              Text("Route"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.share),
+              Text("Share"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
